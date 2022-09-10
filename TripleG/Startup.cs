7d9cf9c442a40b5aa600ff380d8a03b9f5f15ded
olafire <olafire03@gmail.com>
2022-09-10 03:45:01 +0100

    env 2

--- TripleG/Startup.cs
+++ TripleG/Startup.cs
@@ -80,12 +80,12 @@ namespace TripleG
             env.EnvironmentName = "ASPNETCORE_ENVIRONMENT";
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                // app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
